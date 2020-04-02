b1ac7b6b597bf0be9f9afd48ec28572f7f341ad1
marufahmedtushar <ahmedmaruf471@gmail.com>
2020-04-02 11:46:36 +0600

    first commit

---
+++ mysite/mysite/views.py
@@ -0,0 +1,67 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'index.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
+
+
+def analyze(request):
+    #get the text
+    djtext = request.POST.get('text', 'default')
+
+    #check box values
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+
+
+
+     #check whick  check box is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~|+'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+
+    if(fullcaps =="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+
+    if extraspaceremover =="on":
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1] == " "):
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed extr aspace', 'analyzed_text': analyzed}
+        djtext = analyzed
+
+    if newlineremover == "on":
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char != "\r":
+                analyzed = analyzed + char
+        params = {'purpose': 'Removed newlines', 'analyzed_text': analyzed}
+        djtext = analyzed
+    if (removepunc != "on" and fullcaps != "on" and newlineremover != "on" and extraspaceremover != "on"):
+        return HttpResponse("<h1>You have n't give any  text yet...........</h1>")
+
+    return render(request, 'analyze.html', params)
+
